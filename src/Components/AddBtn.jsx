9b83a5b208a820fbd56d5ee3dce5379314d4adfa
Madd Jido <smileboy1422@gmail.com>
2025-06-17 18:55:44 +0100

    added incrementation

--- src/Components/AddBtn.jsx
+++ src/Components/AddBtn.jsx
@@ -1,25 +1,30 @@
+import { useState } from "react";
+import QuantityBtn from "./QuantityBtn.jsx";
+
 function AddBtn() {
+  const [showQuantity, setShowQuantity] = useState(false);
+
+  function handleShowQuantity() {
+    setShowQuantity(!showQuantity);
+  }
+
   return (
     <>
-      <div className="flex items-center justify-center absolute bottom-[-20px] left-[50%] -translate-x-[50%] w-38 h-10 rounded-3xl  bg-white border-1 border-[hsl(12,20%,44%)] hover:border-[hsl(14,86%,42%)] hover:text-[hsl(14,86%,42%)] hover:cursor-pointer">
-        <img
-          src="./icon-add-to-cart.svg"
-          alt="Add to cart logo"
-          className="w-5 mr-2"
-        />
-        <span className="font-redhatsemibold text-sm">Add to Cart</span>
-      </div>
-
-      {/*      <div className="flex items-center justify-around absolute text-white bottom-[-20px] left-[50%] -translate-x-[50%] w-38 h-10 rounded-3xl  bg-[hsl(14,86%,42%)] border-1  border-[hsl(14,86%,42%)]  hover:cursor-pointer">
-        <div className="h-5 w-5 inline-flex border border-white p-1 rounded-full">
-          <img src="./icon-decrement-quantity.svg" alt="Decrement Logo" />
+      {showQuantity ? (
+        <QuantityBtn />
+      ) : (
+        <div
+          onClick={handleShowQuantity}
+          className="flex items-center justify-center absolute bottom-[-20px] left-[50%] -translate-x-[50%] w-38 md:w-42 h-10 rounded-3xl bg-white border-1 border-[hsl(12,20%,44%)] hover:border-[hsl(14,86%,42%)] hover:text-[hsl(14,86%,42%)] hover:cursor-pointer"
+        >
+          <img
+            src="./icon-add-to-cart.svg"
+            alt="Add to cart logo"
+            className="w-5 mr-2"
+          />
+          <span className="font-redhatsemibold text-sm">Add to Cart</span>
         </div>
-
-        <span className="font-redhatsemibold text-sm">2</span>
-        <div className="h-5 w-5 inline-flex border border-white p-1 rounded-full">
-          <img src="./icon-increment-quantity.svg" alt="Increment Logo" />
-        </div>
-      </div> */}
+      )}
     </>
   );
 }
---
+++ src/Components/QuantityBtn.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function QuantityBtn() {
+  const [count, setCount] = useState(1);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-around absolute text-white bottom-[-20px] left-[50%] -translate-x-[50%] w-38 md:w-42 h-10 rounded-3xl  bg-[hsl(14,86%,42%)] border-1  border-[hsl(14,86%,42%)]  hover:cursor-pointer">
+        <div
+          onClick={decrement}
+          className="h-5 w-5 inline-flex border border-white p-1 rounded-full"
+        >
+          <img src="./icon-decrement-quantity.svg" alt="Decrement Logo" />
+        </div>
+
+        <span className="font-redhatsemibold text-sm">{count}</span>
+        <div
+          onClick={increment}
+          className="h-5 w-5 inline-flex border border-white p-1 rounded-full"
+        >
+          <img src="./icon-increment-quantity.svg" alt="Increment Logo" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default QuantityBtn;
