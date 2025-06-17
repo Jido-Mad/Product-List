f94142d9d0adbdb768eff5f8fb2770037a001eed
Madd Jido <smileboy1422@gmail.com>
2025-06-17 16:06:51 +0100

    added styling to the products interface

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,10 +1,25 @@
+import AddBtn from "./AddBtn.jsx";
+
 const ProductCard = ({ product }) => {
   return (
-    <div>
-      <img src={product.image.desktop} alt={product.name} />
-      <h3>{product.name}</h3>
-      <p>{product.category}</p>
-      <p>${product.price.toFixed(2)}</p>
+    <div className="my-6">
+      <div className="relative">
+        <AddBtn />
+        <img
+          src={product.image.mobile}
+          alt={product.name}
+          className="rounded-lg mb-10"
+        />
+      </div>
+      <h3 className="text-sm font-redhatthin text-[hsl(12,20%,44%)]">
+        {product.category}
+      </h3>
+      <p className="font-redhatsemibold text-[hsl(12,20%,15%)]">
+        {product.name}
+      </p>
+      <p className="text-[hsl(14,86%,42%)] font-redhatsemibold">
+        ${product.price.toFixed(2)}
+      </p>
     </div>
   );
 };
