d68a4e57c2d118dd72308c3ba9f4dd017976d5d0
Madd Jido <smileboy1422@gmail.com>
2025-06-18 12:37:27 +0100

    Image Change depending on Window Width + return to add to cart button if quntity is less than 1

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -8,7 +8,7 @@ const Products = () => {
       <h1 className="text-4xl font-redhatbold mb-4 text-[hsl(14,65%,9%))]">
         Desserts
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6">
         {data.map((product) => (
           <ProductCard
             key={`${product.name}-${product.category}`}
