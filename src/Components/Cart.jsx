20145e4afd3bc5a588a530ecb340c7efa6eb62db
n1ro23 <angmunir@gmail.com>
2025-06-21 14:24:17 +0100

    done

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -49,56 +49,6 @@ function Cart() {
               />
             </div>
           </div>
-          <div className="flex justify-between items-center">
-            <div className="space-y-2">
-              <p className="font-redhatsemibold text-[12px] text-gray-800">
-                Vanilla Bean Crème Brûlée
-              </p>
-              <div className="flex space-x-2">
-                <p className="font-redhatbold text-[10px] text-[hsl(14,86%,42%)]">
-                  4x
-                </p>
-                <p className="font-redhatsemibold text-[10px] text-[hsl(7,20%,60%)]">
-                  @ $7.00
-                </p>
-                <p className="font-redhatsemibold text-[10px] text-[hsl(12,20%,44%)]">
-                  $28.00
-                </p>
-              </div>
-            </div>
-            <div className="w-3 h-3 border-1 border-[hsl(14,25%,72%)] rounded-full flex items-center justify-center">
-              <img
-                src="./icon-remove-item.svg"
-                alt="icon remove item"
-                className="w-2 h-2"
-              />
-            </div>
-          </div>
-          <div className="flex justify-between items-center">
-            <div className="space-y-2">
-              <p className="font-redhatsemibold text-[12px] text-gray-800">
-                Vanilla Panna Cotta
-              </p>
-              <div className="flex space-x-2">
-                <p className="font-redhatbold text-[10px] text-[hsl(14,86%,42%)]">
-                  2x
-                </p>
-                <p className="font-redhatsemibold text-[10px] text-[hsl(7,20%,60%)]">
-                  @ $6.50
-                </p>
-                <p className="font-redhatsemibold text-[10px] text-[hsl(12,20%,44%)]">
-                  $13.00
-                </p>
-              </div>
-            </div>
-            <div className="w-3 h-3 border-1 border-[hsl(14,25%,72%)] rounded-full flex items-center justify-center">
-              <img
-                src="./icon-remove-item.svg"
-                alt="icon remove item"
-                className="w-2 h-2"
-              />
-            </div>
-          </div>
         </div>
 
         <div className="flex justify-between items-center mt-8">
