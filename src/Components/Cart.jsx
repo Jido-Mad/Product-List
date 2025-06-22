39231d05a4a27700eaaafc6b63f762e98c94a600
Jido Mad <smileboy1422@gmail.com>
2025-06-22 18:10:02 +0100

    testing

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,7 +1,6 @@
 function Cart() {
   return (
     <>
-      {/* empty cart */}
       <div className=" hidden bg-white flex-col justify-center rounded-lg items-center w-[90%] sm:w-90 pb-6 my-6 lg:mr-4">
         <div className="w-full">
           <h1 className=" flex justify-start font-redhatbold text-[hsl(14,86%,42%)] text-xl text-left p-4 mb-8">
